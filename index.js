225e42c613f2f58599e1c43a89a69af1e4e0e726
Nikolay <nikolay.frontend@gmail.com>
2022-05-11 00:13:38 +0300

    refactor: Virtual Keyboard

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const windowOc = document.createElement('p');
 windowOc.textContent = 'Клавиатура создана в операционной системе Windows';
 const combination = document.createElement('p');
 combination.textContent = 'Для переключения языка комбинация: левыe shift + alt';
-note.append(windowOc, combination);
+const es = document.createElement('p');
+es.textContent = 'ES6: forEach, параметры по умолчанию, стрелочные функции, деструктуризация, интерполяция строк';
+note.append(windowOc, combination, es);
 document.body.prepend(title, textarea, keyboard, note);
 
 const en = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-',
@@ -48,7 +50,7 @@ const enCaps = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-',
   'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M',
   ',', '.', '/', '▲', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt',
   '◄', '▼', '►', 'Ctrl'];
-const ruCaps = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-',
+const ruCaps = ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-',
   '=', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З',
   'Х', 'Ъ', '\\', 'del', 'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л',
   'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б',
@@ -90,44 +92,33 @@ function createButton(char, clas = null) {
   return div;
 }
 
-function createKeyboard(lang, shiftDown = false, caps = false) {
+function createKeyboard(language, shiftDown = false, caps = false) {
   let char;
   if (shiftDown) {
-    char = lang ? enShift : ruShift;
+    char = language ? enShift : ruShift;
   } else if (caps) {
-    char = lang ? enCaps : ruCaps;
+    char = language ? enCaps : ruCaps;
   } else {
-    char = lang ? en : ru;
+    char = language ? en : ru;
   }
 
-  for (let i = 0; i < char.length; i++) {
-    if (char[i] === 'Backspace') {
-      keyboard.append(createButton(char[i], 'backspace'));
-      continue;
+  char.forEach((item) => {
+    if (item === 'Backspace') {
+      keyboard.append(createButton(item, 'backspace'));
+    } else if (item === 'Tab') {
+      keyboard.append(createButton(item, 'tab'));
+    } else if (item === 'CapsLock') {
+      keyboard.append(createButton(item, 'caps'));
+    } else if (item === 'Enter') {
+      keyboard.append(createButton(item, 'enter'));
+    } else if (item === 'Shift') {
+      keyboard.append(createButton(item, 'shift'));
+    } else if (item === ' ') {
+      keyboard.append(createButton(item, 'spaсe'));
+    } else {
+      keyboard.append(createButton(item));
     }
-    if (char[i] === 'Tab') {
-      keyboard.append(createButton(char[i], 'tab'));
-      continue;
-    }
-    if (char[i] === 'CapsLock') {
-      keyboard.append(createButton(char[i], 'caps'));
-      continue;
-    }
-    if (char[i] === 'Enter') {
-      keyboard.append(createButton(char[i], 'enter'));
-      continue;
-    }
-    if (char[i] === 'Shift') {
-      keyboard.append(createButton(char[i], 'shift'));
-      continue;
-    }
-    if (char[i] === ' ') {
-      keyboard.append(createButton(char[i], 'spaсe'));
-      continue;
-    }
-    keyboard.append(createButton(char[i]));
-  }
-
+  });
   return keyboard;
 }
 createKeyboard(lang);
@@ -141,7 +132,6 @@ const changeLang = (e) => {
     } else {
       localStorage.setItem('lang', '');
     }
-    const keyboard = document.querySelector('.keyboard');
     keyboard.innerHTML = '';
 
     createKeyboard(lang);
@@ -217,96 +207,183 @@ const ruShiftUp = (e) => {
 };
 document.addEventListener('keyup', ruShiftUp);
 
-let upper = false;
 const textOut = (e) => {
   e.preventDefault();
-  if (upper) {
+  if (flag) {
+    if (e.key === 'Backspace') {
+      const str = textarea.value;
+      textarea.value = textarea.value.slice(0, str.length - 1);
+      return textarea.value;
+    }
+    if (e.key === 'Delete') {
+      textarea.value = textarea.value.slice(0, textarea.selectionStart)
+       + textarea.value.slice(textarea.selectionStart + 1);
+      return textarea.value;
+    }
+    if (e.key === 'Tab') {
+      textarea.value += '  ';
+      return textarea.value;
+    }
+    if (e.key === 'ArrowLeft') {
+      textarea.value += '◄';
+      return textarea.value;
+    }
+    if (e.key === 'ArrowRight') {
+      textarea.value += '►';
+      return textarea.value;
+    }
+    if (e.key === 'ArrowUp') {
+      textarea.value += '▲';
+      return textarea.value;
+    }
+    if (e.key === 'ArrowDown') {
+      textarea.value += '▼';
+      return textarea.value;
+    }
+    if (e.key === 'Shift' || e.key === 'Enter' || e.key === 'Control'
+    || e.key === 'Delete' || e.key === 'CapsLock' || e.key === 'Tab'
+    || e.key === 'Alt' || e.key === 'Meta' || e.key === 'ArrowLeft'
+    || e.key === 'ArrowRight' || e.key === 'ArrowUp' || e.key === 'ArrowDown'
+    || e.key === 'Backspace' || e.key === 'AltGraph') {
+      return e.key;
+    }
     const a = e.key.toUpperCase();
-    return textarea.value += a;
+    textarea.value += a;
+    return textarea.value;
   }
   if (e.key === 'Backspace') {
     const str = textarea.value;
-    return textarea.value = textarea.value.slice(0, str.length - 1);
+    textarea.value = textarea.value.slice(0, str.length - 1);
+    return textarea.value;
   }
 
   if (e.key === 'Delete') {
-    return textarea.value = textarea.value.slice(0, textarea.selectionStart)
+    textarea.value = textarea.value.slice(0, textarea.selectionStart)
      + textarea.value.slice(textarea.selectionStart + 1);
+    return textarea.value;
   }
 
   if (e.key === 'Tab') {
-    return textarea.value += '  ';
+    textarea.value += '  ';
+    return textarea.value;
   }
 
   if (e.key === 'ArrowLeft') {
-    return textarea.value += '◄';
+    textarea.value += '◄';
+    return textarea.value;
   }
   if (e.key === 'ArrowRight') {
-    return textarea.value += '►';
+    textarea.value += '►';
+    return textarea.value;
   }
   if (e.key === 'ArrowUp') {
-    return textarea.value += '▲';
+    textarea.value += '▲';
+    return textarea.value;
   }
   if (e.key === 'ArrowDown') {
-    return textarea.value += '▼';
+    textarea.value += '▼';
+    return textarea.value;
   }
 
   if (e.key === 'Shift' || e.key === 'Enter' || e.key === 'Control'
   || e.key === 'Del' || e.key === 'CapsLock' || e.key === 'Tab'
-  || e.key === 'Alt' || e.key === 'Meta') {
-    return;
+  || e.key === 'Alt' || e.key === 'AltGraph' || e.key === 'Meta') {
+    return e.key;
   }
-
-  return textarea.value += e.key;
+  if (e.shiftKey && e.key) {
+    textarea.value += e.key;
+    return textarea.value;
+  }
+  textarea.value += e.key.toLowerCase();
+  return textarea.value;
 };
 document.addEventListener('keydown', textOut);
 
 const backlightOn = (e) => {
+  if (e.key === 'CapsLock') {
+    return;
+  }
   let char;
   if (e.key === '\\') {
     char = document.querySelector('[data-atribute="BackSlash"]');
-  } else if (upper) {
-    const upperCase = e.key.toUpperCase();
-    char = document.querySelector(`[data-atribute="${upperCase}"`);
+  } else if (e.code === 'AltRight') {
+    [, char] = document.querySelectorAll('[data-atribute="Alt"]');
+  } else if (flag) {
+    if (e.key === 'Shift' || e.key === 'Enter' || e.key === 'Control'
+    || e.key === 'Delete' || e.key === 'Tab'
+    || e.key === 'Alt' || e.key === 'Meta' || e.key === 'ArrowLeft'
+    || e.key === 'ArrowRight' || e.key === 'ArrowUp' || e.key === 'ArrowDown'
+    || e.key === 'Backspace') {
+      char = document.querySelector(`[data-atribute="${e.key}"`);
+    } else {
+      const upperCase = e.key.toUpperCase();
+      char = document.querySelector(`[data-atribute="${upperCase}"`);
+    }
   } else if (e.key === '"') {
     char = document.querySelector('[data-atribute="DoubleQuotes"]');
-  } else {
+  } else if (e.key === 'ArrowLeft'
+    || e.key === 'ArrowRight' || e.key === 'ArrowUp' || e.key === 'ArrowDown') {
     char = document.querySelector(`[data-atribute="${e.key}"`);
+  } else if (e.shiftKey && e.key) {
+    char = document.querySelector(`[data-atribute="${e.key}"`);
+    char.classList.add('backlight');
+  } else {
+    const lowerCase = e.key.length > 1 ? e.key[0].toUpperCase()
+    + e.key.slice(1, e.key.length).toLowerCase() : e.key.toLowerCase();
+    char = document.querySelector(`[data-atribute="${lowerCase}"`);
   }
-  return char.classList.add('backlight');
+  char.classList.add('backlight');
 };
 document.addEventListener('keydown', backlightOn);
 
 const backlightOff = (e) => {
   if (e.code === 'AltRight' || e.code === 'ControlRight') {
+    if (e.key === 'AltGraph') {
+      const char = document.querySelectorAll('[data-atribute="Alt"]')[1];
+      return char.classList.remove('backlight');
+    }
     const char = document.querySelectorAll(`[data-atribute="${e.key}"`)[1];
-    char.classList.remove('backlight');
-    return;
+    return char.classList.remove('backlight');
   }
 
   if (e.key === 'CapsLock') {
-    return;
+    return e.key;
   }
 
   if (e.key === '\\') {
     const char = document.querySelector('[data-atribute="BackSlash"]');
-    char.classList.remove('backlight');
-    return;
+    return char.classList.remove('backlight');
   }
   if (e.key === '"') {
     const char = document.querySelector('[data-atribute="DoubleQuotes"]');
-    char.classList.remove('backlight');
-    return;
+    return char.classList.remove('backlight');
   }
-  if (upper) {
+  if (flag) {
+    if (e.key === 'Shift' || e.key === 'Enter' || e.key === 'Control'
+    || e.key === 'Delete' || e.key === 'CapsLock' || e.key === 'Tab'
+    || e.key === 'Alt' || e.key === 'Meta' || e.key === 'ArrowLeft'
+    || e.key === 'ArrowRight' || e.key === 'ArrowUp' || e.key === 'ArrowDown'
+    || e.key === 'Backspace' || e.key === 'AltGraph') {
+      const char = document.querySelector(`[data-atribute="${e.key}"`);
+      return char.classList.remove('backlight');
+    }
     const upperCase = e.key.toUpperCase();
     const char = document.querySelector(`[data-atribute="${upperCase}"`);
-    char.classList.remove('backlight');
-    return;
+    return char.classList.remove('backlight');
   }
-
-  const char = document.querySelector(`[data-atribute="${e.key}"`);
-  char.classList.remove('backlight');
+  if (e.key === 'ArrowLeft'
+  || e.key === 'ArrowRight' || e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+    const char = document.querySelector(`[data-atribute="${e.key}"`);
+    return char.classList.remove('backlight');
+  }
+  if (e.shiftKey && e.key) {
+    const char = document.querySelector(`[data-atribute="${e.key}"`);
+    return char.classList.add('backlight');
+  }
+  const lowerCase = e.key.length > 1 ? e.key[0].toUpperCase()
+  + e.key.slice(1, e.key.length).toLowerCase() : e.key.toLowerCase();
+  const char = document.querySelector(`[data-atribute="${lowerCase}"`);
+  return char.classList.remove('backlight');
 };
 document.addEventListener('keyup', backlightOff);
 
@@ -317,24 +394,21 @@ const clickTextOut = (e) => {
 
   if (e.target.textContent === 'Backspace') {
     const str = textarea.value;
-    return textarea.value = textarea.value.slice(0, str.length - 1);
-  }
-
-  if (e.target.textContent === 'del') {
-    return textarea.value = textarea.value.slice(0, textarea.selectionStart)
-    + textarea.value.slice(textarea.selectionStart + 1);
-  }
-  if (e.target.textContent === 'Tab') {
-    return textarea.value += '  ';
-  }
-  if (e.target.textContent === 'Shift' || e.target.textContent === 'Enter'
+    textarea.value = textarea.value.slice(0, str.length - 1);
+  } else if (e.target.textContent === 'del') {
+    const delBefore = textarea.value.slice(0, textarea.selectionStart);
+    const delAfter = textarea.value.slice(textarea.selectionStart + 1);
+    textarea.value = delBefore + delAfter;
+  } else if (e.target.textContent === 'Tab') {
+    textarea.value += '  ';
+  } else if (e.target.textContent === 'Shift' || e.target.textContent === 'Enter'
   || e.target.textContent === 'Ctrl' || e.target.textContent === 'del'
   || e.target.textContent === 'CapsLock' || e.target.textContent === 'Tab'
   || e.target.textContent === 'Alt' || e.target.textContent === 'Win') {
-    return;
+    textarea.value += '';
+  } else {
+    textarea.value += e.target.textContent;
   }
-
-  textarea.value += e.target.textContent;
 };
 document.addEventListener('click', clickTextOut);
 
@@ -352,6 +426,8 @@ const capsDown = (e) => {
   } else {
     keyboard.innerHTML = '';
     createKeyboard(lang, false, false);
+    const char = document.querySelector(`[data-atribute="${e.key}"`);
+    char.classList.remove('backlight');
   }
 };
 document.addEventListener('keydown', capsDown);
@@ -361,8 +437,8 @@ const enterClick = (e) => {
     return;
   }
   if (e.target.textContent === 'Enter') {
-    textarea.value = textarea.value.slice(0, textarea.selectionStart)
-    + '\n' + textarea.value.slice(textarea.selectionStart);
+    const enterBefore = textarea.value.slice(0, textarea.selectionStart);
+    textarea.value = `${enterBefore}\n${textarea.value.slice(textarea.selectionStart)}`;
   }
 };
 document.addEventListener('click', enterClick);
@@ -383,8 +459,9 @@ const enterKey = (e) => {
   }
 
   if (e.key === 'Enter') {
-    textarea.value = textarea.value.slice(0, textarea.selectionStart)
-    + '\n' + textarea.value.slice(textarea.selectionStart);
+    const enterBefore = textarea.value.slice(0, textarea.selectionStart);
+    const enterAfter = textarea.value.slice(textarea.selectionStart);
+    textarea.value = `${enterBefore}\n${enterAfter}`;
   }
 };
 document.addEventListener('keydown', enterKey);
@@ -402,6 +479,9 @@ document.addEventListener('keydown', shiftRightKeyBackLight);
 
 const altRightKeyBackLight = (e) => {
   if (e.code !== 'AltRight') {
+    return;
+  }
+  if (e.key === 'AltGraph') {
     return;
   }
   const keyLeft = document.querySelectorAll(`[data-atribute="${e.key}"`)[0];
@@ -434,7 +514,6 @@ const clickChangeLang = (e) => {
     } else {
       localStorage.setItem('lang', '');
     }
-    const keyboard = document.querySelector('.keyboard');
     keyboard.innerHTML = '';
 
     createKeyboard(lang);
@@ -477,7 +556,6 @@ const mouseShiftDown = (e) => {
 
   keyboard.innerHTML = '';
   createKeyboard(lang, true);
-  upper = true;
 
   const shift = document.querySelectorAll('.shift');
   if (e.clientX < 1000) {
@@ -505,7 +583,6 @@ const mouseShiftUp = (e) => {
 
   keyboard.innerHTML = '';
   createKeyboard(lang, false);
-  upper = false;
 
   if (flag) {
     keyboard.innerHTML = '';
